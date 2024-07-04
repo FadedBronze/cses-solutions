92654e1a94cc5b77ee1c6fee8c416cdc9c20f050
FadedBronze <TheSamyatGautam@gmail.com>
2024-07-04 17:08:58 -0400

    two_knights solved

--- two_knights.cpp
+++ two_knights.cpp
@@ -4,57 +4,10 @@
 int main() {
   std::string line_1;
   std::getline(std::cin, line_1, '\n');
+  long long n = std::stoi(line_1);
 
-  int n = std::stoi(line_1);
-  int nn = n * n;
-
-  int total_possible = 0;
-  
-  for (int y = 0; y < n; y++) {
-    for (int x = 0; x < n; x++) {
-      int possible_spaces = nn - (y * n + x) - 1;
-
-      bool vspace2 = y + 2 < n;
-      bool vspace1 = vspace2 || y + 1 < n;
-      bool vspacen2 = false;
-      bool vspacen1 = false;
-      
-      bool hspace2 = x + 2 < n;
-      bool hspace1 = hspace2 || x + 1 < n;
-      bool hspacen2 = x - 2 >= 0;
-      bool hspacen1 = hspacen2 || x - 1 >= 0;
-
-      if (vspace2) {
-        if (hspace1) 
-          possible_spaces--;
-        if (hspacen1) 
-          possible_spaces--;
-      }
-
-      if (vspacen2) {
-        if (hspace1) 
-          possible_spaces--;
-        if (hspacen1) 
-          possible_spaces--;
-      }
-
-      if (hspace2) {
-        if (vspace1) 
-          possible_spaces--;
-        if (vspacen1) 
-          possible_spaces--;
-      }
-
-      if (hspacen2) {
-        if (vspace1) 
-          possible_spaces--;
-        if (vspacen1) 
-          possible_spaces--;
-      }
-
-      total_possible += possible_spaces;
-    }
+  for (long long i = 1; i < n+1; i++) {
+    long long result = (((i*i) * (i*i-1) / 2) - 2 * ((i-1) * (i-2)) - 2 * ((i-1) * (i-2)));
+    std::cout << result << std::endl; 
   }
-
-  std::cout << total_possible << '\n';
 }
